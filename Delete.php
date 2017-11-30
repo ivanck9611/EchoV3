852e2b1c5d4ca59b2faf6e6ef45c14761f5b7377
shakehead <kaiyungli@gmail.com>
2017-11-30 14:06:28 +0800

    update delete function

--- Delete.php
+++ Delete.php
@@ -32,12 +32,12 @@ $pri = $_POST['price'];
 	elseif($c == 'speaker')
 	{
 
-		$query = "SELECT Imagepath from woman where Product_id = '$p'";
+		$query = "SELECT path from speaker where id = '$p'";
 		$r = mysqli_query($db, $query);
 		$r2 = mysqli_fetch_array($r);
-		$string =  $r2['Imagepath'];
-		$newstring = str_replace('/PJ/','',$string);
-		$query = "DELETE from woman where Product_id = '$p'";
+		$string =  $r2['path'];
+		$newstring = str_replace('/img/poducts/speaker/','',$string);
+		$query = "DELETE from speaker where id = '$p'";
 
 		$l2 = mysqli_query($db,$query);
 
@@ -53,15 +53,15 @@ $pri = $_POST['price'];
 
 }
 
-else if($c == 'router')
+elseif($c == 'hifi')
 {
 
-	$query = "SELECT Imagepath from woman where Product_id = '$p'";
+	$query = "SELECT path from hifi where id = '$p'";
 	$r = mysqli_query($db, $query);
 	$r2 = mysqli_fetch_array($r);
-	$string =  $r2['Imagepath'];
-	$newstring = str_replace('/PJ/','',$string);
-	$query = "DELETE from woman where Product_id = '$p'";
+	$string =  $r2['path'];
+	$newstring = str_replace('/img/poducts/hifi/','',$string);
+	$query = "DELETE from hifi where id = '$p'";
 
 	$l2 = mysqli_query($db,$query);
 
@@ -76,6 +76,102 @@ else if($c == 'router')
 	}
 
 }
+
+elseif($c == 'portable speaker')
+{
+
+	$query = "SELECT path from portable speaker where id = '$p'";
+	$r = mysqli_query($db, $query);
+	$r2 = mysqli_fetch_array($r);
+	$string =  $r2['path'];
+	$newstring = str_replace('/img/poducts/portable speaker/','',$string);
+	$query = "DELETE from portable speaker where id = '$p'";
+
+	$l2 = mysqli_query($db,$query);
+
+	if($l2)
+	{
+		echo "<h2> Successfully Deleted.........";
+		unlink($newstring);
+	}
+	else
+	{
+		echo "<h2> Failed to delete...............";
+	}
+
+}
+
+elseif($c == 'headphone')
+{
+
+	$query = "SELECT path from headphone where id = '$p'";
+	$r = mysqli_query($db, $query);
+	$r2 = mysqli_fetch_array($r);
+	$string =  $r2['path'];
+	$newstring = str_replace('/img/poducts/headphone/','',$string);
+	$query = "DELETE from path where id = '$p'";
+
+	$l2 = mysqli_query($db,$query);
+
+	if($l2)
+	{
+		echo "<h2> Successfully Deleted.........";
+		unlink($newstring);
+	}
+	else
+	{
+		echo "<h2> Failed to delete...............";
+	}
+
+}
+elseif($c == 'keyboard')
+{
+
+	$query = "SELECT path from keyboard where id = '$p'";
+	$r = mysqli_query($db, $query);
+	$r2 = mysqli_fetch_array($r);
+	$string =  $r2['path'];
+	$newstring = str_replace('/img/poducts/keyboard/','',$string);
+	$query = "DELETE from keyboard where id = '$p'";
+
+	$l2 = mysqli_query($db,$query);
+
+	if($l2)
+	{
+		echo "<h2> Successfully Deleted.........";
+		unlink($newstring);
+	}
+	else
+	{
+		echo "<h2> Failed to delete...............";
+	}
+
+}
+
+elseif($c == 'mouse')
+{
+
+	$query = "SELECT path from mouse where id = '$p'";
+	$r = mysqli_query($db, $query);
+	$r2 = mysqli_fetch_array($r);
+	$string =  $r2['path'];
+	$newstring = str_replace('/img/poducts/mouse/','',$string);
+	$query = "DELETE from mouse where id = '$p'";
+
+	$l2 = mysqli_query($db,$query);
+
+	if($l2)
+	{
+		echo "<h2> Successfully Deleted.........";
+		unlink($newstring);
+	}
+	else
+	{
+		echo "<h2> Failed to delete...............";
+	}
+
+}
+
 
 mysqli_close($db);
 ?>
